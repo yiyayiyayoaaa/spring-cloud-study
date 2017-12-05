e2e92768832bae95d55025829e75c7b9965590db
cheng.xiao <cheng.xiao@microcardio.com>
2017-12-05 17:44:37 +0800

    配置中心

--- config-server-git/src/main/java/com/microcardio/configservergit/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/microcardio/configservergit/ConfigServerGitApplication.java
@@ -1,12 +1,14 @@
 package com.microcardio.configservergit;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerGitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerGitApplication.class, args);
+		new SpringApplicationBuilder(ConfigServerGitApplication.class).web(true).run(args);
 	}
 }
